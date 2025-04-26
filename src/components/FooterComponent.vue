33382f4607f8e3b822ca1258061434d242732c06
Adrián Catalán <adrianncatalan@gmail.com>
2025-04-26 22:57:07 +0200

    more changes by lulu

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,7 +2,7 @@
     <footer id="footer" class="bg-black">
         <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
             <div class="mt-8 flex justify-center space-x-6">
-                <a href="#"
+                <a href="https://hubertfroehlich.netlify.app/"
                    class="text-indigo-600 hover:text-white hover:bg-indigo-600 text-center hover:shadow-lg px-4 py-2 rounded-lg transition-all duration-300 transform hover:scale-105">
                     Erlebnisbauernhof Hubert Fröhlich
                 </a>
