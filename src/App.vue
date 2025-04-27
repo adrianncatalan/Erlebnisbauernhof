7fb93b76dc2f4eb6bf67665ebc34dc5c1bfe3b81
Adrián Catalán <adrianncatalan@gmail.com>
2025-04-27 12:11:43 +0200

    small changes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import ContactComponent from './components/ContactComponent.vue';
-import FooterComponent from './components/FooterComponent.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
+import ContactComponent from './components/ContactComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 </script>
 
 <template>
