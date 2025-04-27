4a7cc4c28bcc367d6727ddc6e72ace04edfcc435
Adrián Catalán <adrianncatalan@gmail.com>
2025-04-27 12:10:47 +0200

    small changes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import ContactComponent from './components/ContactComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
-import HeaderSection from './components/HeaderSection.vue';
-import MainSection from './components/MainSection.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 </script>
 
 <template>
-  <HeaderSection />
-  <MainSection />
+  <HeaderComponent />
+  <MainComponent />
   <ContactComponent />
   <FooterComponent />
 </template>
