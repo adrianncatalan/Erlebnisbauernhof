93f23a02fa6cad8e0049bd780d62ad8a45bd6dcc
Adrián Catalán <adrianncatalan@gmail.com>
2025-06-22 15:49:49 +0200

    Adding a new image

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,30 +1,38 @@
 <template>
-    <div class="bg-white py-24 sm:py-32">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-            <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+  <div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
 
-                <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-                    <dt class="text-2xl font-bold text-slate-500">Zertifizierter bayerischer</dt>
-                    <dd class="text-3xl font-semibold tracking-tight text-slate-950 sm:text-5xl">
-                        Baumwart
-                    </dd>
-                </div>
-
-                <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-                    <dt class="text-2xl font-bold text-slate-500">Staatlich geprüfter</dt>
-                    <dd class="text-3xl font-semibold tracking-tight text-slate-950 sm:text-5xl">
-                        Brenner
-                    </dd>
-                </div>
-
-                <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-                    <dt class="text-2xl font-bold text-slate-500">Paracelsus</dt>
-                    <dd class="text-3xl font-semibold tracking-tight text-slate-950 sm:text-5xl">
-                        Psychologischer Berater
-                    </dd>
-                </div>
-
-            </dl>
+        <!-- Block 1 -->
+        <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+          <dt class="text-2xl font-bold text-slate-500">Zertifizierter bayerischer</dt>
+          <dd class="text-3xl font-semibold tracking-tight text-slate-950 sm:text-5xl">
+            Baumwart
+          </dd>
         </div>
+
+        <!-- Block 2 -->
+        <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+          <dt class="text-2xl font-bold text-slate-500">Staatlich geprüfter</dt>
+          <dd class="text-3xl font-semibold tracking-tight text-slate-950 sm:text-5xl">
+            Brenner
+          </dd>
+        </div>
+
+        <!-- Block 3 -->
+        <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+          <dt class="text-2xl font-bold text-slate-500">Paracelsus</dt>
+          <dd class="text-3xl font-semibold tracking-tight text-slate-950 sm:text-5xl">
+            Psychologischer Berater
+          </dd>
+        </div>
+      </dl>
+
+      <!-- Image at the end with fixed size and margin -->
+      <div class="col-span-1 lg:col-span-3">
+        <img src="https://i.postimg.cc/pTBwkqLq/main-image.jpg" alt="Illustration"
+          class="mx-auto mt-8 mb-4 rounded-xl shadow-lg object-cover w-xs" />
+      </div>
     </div>
+  </div>
 </template>
